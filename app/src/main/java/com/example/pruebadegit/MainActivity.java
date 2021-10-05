5cfb591ae67ee022fbee583b0d027084f85cc369
Franco <francogimenez981@gmail.com>
2021-10-05 09:12:29 -0300

    agrego suma

--- app/src/main/java/com/example/pruebadegit/MainActivity.java
+++ app/src/main/java/com/example/pruebadegit/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.pruebadegit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //asdf
-        int a=0;
+        int a=1 ,b=2 , c;
+
+        c=a+b;
+
+
+
     }
 }
